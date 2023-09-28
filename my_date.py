ae99b7f7f3b791433e8183849728e74475e63a18
Saikiranchenn <chennas@mail.gvsu.edu>
2023-09-27 23:04:26 -0400

    first

--- my_date.py
+++ my_date.py
@@ -15,11 +15,27 @@ def is_leap_year(year: int) -> bool:
     else:
         return False
 
+def ordinal_date(year: int, month: int, day: int) -> int:
+    """Return the number of days elapsed since the beginning of the year, including any partial days."""
+    # Number of days in each month (non-leap year)
+    days_in_month = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+    # Update February if it's a leap year
+    if is_leap_year(year):
+        days_in_month[2] = 29
+
+    # Calculate the ordinal date
+    ordinal_date = day
+    for i in range(1, month):
+        ordinal_date += days_in_month[i]
+
+    return ordinal_date
+
 def ordinal_date(year:int , month: int, day: int) -> int:
     """ Return the number of days elapsed since the beginning of the year, including any partial days.
         For example, the ordinal date for 1 January is 1.
         Hint: pre-compute the ordinal date for the first of each month."""
-    pass
+    
 
 def days_elapsed(year1: int, month1: int, day1: int, year2: int, month2: int, day2:int ) -> int:
     """ Return the number of days that have elapsed between year1-month1-day1 and year2-month2-day2.
