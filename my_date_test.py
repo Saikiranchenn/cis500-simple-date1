0a6b9926abd9d207f3936a9d3381a647a38a66c4
Saikiranchenn <chennas@mail.gvsu.edu>
2023-09-27 22:37:02 -0400

    latest changes

--- my_date_test.py
+++ my_date_test.py
@@ -2,50 +2,24 @@ import unittest
 import my_date
 
 class MyDateTest(unittest.TestCase):
+    def test_is_leap_year(self):
+        test_cases = [
+            (2000, True),
+            (2004, True),
+            (2100, False),
+            (2023, False),
+            (1600, True),
+            (1900, False),
+            (2008, True),
+            (2400, True),
+            (2800, True),
+            (4000, True),
+        ]
 
+        for year, expected_output in test_cases:
+            actual_output = is_leap_year(year)
+            self.assertEqual(expected_output, actual_output)
 
-  def test_is_leap_year_divisible_by_4(self):
-
-    self.assertTrue(my_date.is_leap_year(2000))
-
-    self.assertTrue(my_date.is_leap_year(2004))
-
-    self.assertTrue(my_date.is_leap_year(2008))
-
-
-  def test_is_leap_year_not_divisible_by_100(self):
-
-    self.assertTrue(my_date.is_leap_year(2000))
-
-    self.assertTrue(my_date.is_leap_year(2004))
-
-    self.assertTrue(my_date.is_leap_year(2008))
-
-
-
-  def test_is_leap_year_divisible_by_100_and_400(self):
-
-    self.assertTrue(my_date.is_leap_year(1600))
-
-    self.assertTrue(my_date.is_leap_year(2000))
-
-    self.assertTrue(my_date.is_leap_year(2400))
-
-
-
-  def test_is_leap_year_not_divisible_by_100_and_400(self):
-
-    self.assertFalse(my_date.is_leap_year(1900))
-
-    self.assertFalse(my_date.is_leap_year(2100))
-
-    self.assertFalse(my_date.is_leap_year(2200))
-
-
-
-
-
-    
 
 if __name__ == '__main__':
     unittest.main()
