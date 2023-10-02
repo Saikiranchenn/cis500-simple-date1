b341555a24327d78b9106affcbd38508000fa5ce
Saikiranchenn <chennas@mail.gvsu.edu>
2023-10-02 18:20:10 -0400

    Update my_date_test.py

--- my_date_test.py
+++ my_date_test.py
@@ -2,93 +2,52 @@ import unittest
 import my_date
 
 class MyDateTest(unittest.TestCase):
-    def test_is_leap_year(self):
-        test_cases = [
-            (2000, True),
-            (2004, True),
-            (2100, False),
-            (2023, False),
-            (1600, True),
-            (1900, False),
-            (2008, True),
-            (2400, True),
-            (2800, True),
-            (4000, True),
-        ]
 
-        for year, expected_output in test_cases:
-            actual_output = is_leap_year(year)
-            self.assertEqual(expected_output, actual_output)
+    #
+    # is_leap_year
+    #
 
+    def test_is_leap_year1(self):
+        self.assertTrue(my_date.is_leap_year(1984))
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_is_leap_year2(self):
+        self.assertFalse(my_date.is_leap_year(1985))
 
-# Test cases for the general_date() function
+    #
+    # ordinal_date
+    #
 
-# Valid dates
-general_date(2023, 1, 1) == 1
-general_date(2023, 3, 8) == 67
-general_date(2023, 12, 31) == 365
+    def test_ordinal_date1(self):
+        self.assertEqual(1, my_date.ordinal_date(1997, 1, 1))
 
-# Leap year
-general_date(2024, 2, 29) == 60
+    def test_ordinal_date2(self):
+        self.assertEqual(32, my_date.ordinal_date(1995, 2, 1))
 
-# Edge cases
-general_date(2023, 0, 1) == 1
-general_date(2023, 13, 1) == 365
+    #
+    # days_elapsed  
+    #
 
-# Invalid dates
-general_date(2023, -1, 1) == -1
-general_date(2023, 14, 1) == -1
-general_date(2023, 1, 32) == -1
+    def test_days_elapsed1(self):
+        self.assertEqual(1, my_date.days_elapsed(1997, 1, 1, 1997, 1, 2))
 
-# Non-integer inputs
-general_date(2.5, 1, 1) == -1
-general_date(1, 2.5, 1) == -1
-general_date(1, 1, 3.5) == -1
-You can use these test cases to verify that the general_date() function is working correctly for all possible inputs.
+    def test_days_elapsed2(self):
+        self.assertEqual(31, my_date.days_elapsed(1997, 1, 1, 1997, 2, 1))
 
-year1 = 2022
-month1 = 12
-day1 = 31
-year2 = 2023
-month2 = 1
-day2 = 1
+    def test_days_elapsed3(self):
+        self.assertEqual(365, my_date.days_elapsed(1997, 1, 1, 1998, 1, 1))
 
-day_of_week(2023, 9, 27) == "Wednesday"
-day_of_week(2024, 2, 29) == "Friday"
-day_of_week(1900, 1, 1) == "Monday"
-day_of_week(2000, 12, 31) == "Saturday"
+    # Warning: You need more tests than you think for this function
 
-day_of_week(2023, 0, 1) == "Invalid date" 
-day_of_week(2023, 13, 1) == "Invalid date" 
-day_of_week(2023, 9, 0) == "Invalid date" 
-day_of_week(2023, 9, 32) == "Invalid date" 
+    #
+    # day_of_week
+    #
 
-import unittest
-
-from to_str import to_str
+    def test_day_of_week1(self):
+        self.assertEqual("Monday", my_date.day_of_week(1753, 1, 1))
 
 
-class TestToStr(unittest.TestCase):
-
-    def test_valid_dates(self):
-        general.commonOne(to_str(2023, 9, 27), "Wednesday, 27 September 2023")
-        general.commonOne(to_str(1833, 3, 7), "Wednesday, 07 March 1833")
-        general.commonOne(to_str(1000, 1, 1), "Saturday, 01 January 1000")
-
-    def test_invalid_dates(self):
-        with general.commonRaises(ValueError):
-            to_str(-1, 9, 27)
-
-        with general.commonRaises(ValueError):
-            to_str(2023, 13, 27)
-
-        with general.commonRaises(ValueError):
-            to_str(2023, 9, 32)
-
-
-if __name__ == '__main__':
-    unittest.main()
-
+    #
+    # to_str
+    #
+    def test_to_str1(self):
+        self.assertEqual("Friday, 02 August 2024", my_date.to_str(2024, 8, 2))
